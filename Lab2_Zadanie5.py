a05fae04c599ef0c7c77a6fb59a33f283b8391c6
aleksandrmoshorovskyi <moshorovskyi.a@icloud.com>
2022-10-24 10:39:23 +0300

    Lab2_Zadanie5

--- Lab2_Zadanie5.py
+++ Lab2_Zadanie5.py
@@ -5,7 +5,19 @@ print('''Jaką operację chcesz wykonać?
 4) dzielenie
 5) potęgowanie''')
 
-dzialanie = int(input("Wpisz numer operacji"))
+dzialanie = int(input("Wpisz numer operacji: "))
 arg1 = float(input('Podaj argument 1: '))
 arg2 = float(input('Podaj argument 2: '))
 
+if dzialanie == 1:
+    w = arg1 + arg2
+elif dzialanie == 2:
+    w = arg1 - arg2
+elif dzialanie == 3:
+    w = arg1 * arg2
+elif dzialanie == 4:
+    w = arg1 / arg2
+elif dzialanie == 5:
+    w = arg1 ** arg2
+
+print(f'Wynik = {w}')
